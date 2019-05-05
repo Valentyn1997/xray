1c21e1472cb45e3889e9c9931413a8812cf82a5d
valentyn <melnychukv@cip.ifi.lmu.de>
2019-05-05 22:44:21 +0200

    Coockiecutter project structure created, 'data/' excluded from git

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Anomaly Detection in X-Ray Images as an Unsupervised learning task',
+    author='random_state42',
+    license='',
+)
