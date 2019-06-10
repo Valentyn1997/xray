98772222a871cf4c62dd6007f27f240d5264273a
DianaDI <dianke402@gmail.com>
2019-06-10 16:50:12 +0200

    refactor CVAE

--- src/models/сvae.py
+++ src/models/сvae.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+
 tf.enable_eager_execution()
 
 
@@ -7,38 +8,38 @@ class CVAE(tf.keras.Model):
         super(CVAE, self).__init__()
         self.latent_dim = latent_dim
         self.inference_net = tf.keras.Sequential(
-          [
-              tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
-              tf.keras.layers.Conv2D(
-                  filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
-              tf.keras.layers.Conv2D(
-                  filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
-              tf.keras.layers.Flatten(),
-              # No activation
-              tf.keras.layers.Dense(latent_dim + latent_dim),
-          ]
+            [
+                tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
+                tf.keras.layers.Conv2D(
+                    filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
+                tf.keras.layers.Conv2D(
+                    filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
+                tf.keras.layers.Flatten(),
+                # No activation
+                tf.keras.layers.Dense(latent_dim + latent_dim),
+            ]
         )
 
         self.generative_net = tf.keras.Sequential(
             [
-              tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
-              tf.keras.layers.Dense(units=7*7*32, activation=tf.nn.relu),
-              tf.keras.layers.Reshape(target_shape=(7, 7, 32)),
-              tf.keras.layers.Conv2DTranspose(
-                  filters=64,
-                  kernel_size=3,
-                  strides=(2, 2),
-                  padding="SAME",
-                  activation='relu'),
-              tf.keras.layers.Conv2DTranspose(
-                  filters=32,
-                  kernel_size=3,
-                  strides=(2, 2),
-                  padding="SAME",
-                  activation='relu'),
-              # No activation
-              tf.keras.layers.Conv2DTranspose(
-                  filters=1, kernel_size=3, strides=(1, 1), padding="SAME"),
+                tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+                tf.keras.layers.Dense(units=7 * 7 * 32, activation=tf.nn.relu),
+                tf.keras.layers.Reshape(target_shape=(7, 7, 32)),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=64,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="SAME",
+                    activation='relu'),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=32,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="SAME",
+                    activation='relu'),
+                # No activation
+                tf.keras.layers.Conv2DTranspose(
+                    filters=1, kernel_size=3, strides=(1, 1), padding="SAME"),
             ]
         )
 
