4be52c792a56e5bbad5f3ee65e3c6a0b20553c87
DianaDI <dianke402@gmail.com>
2019-06-12 09:26:04 +0200

    add torch vae

---
+++ src/models/train-vae.py
@@ -0,0 +1,116 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
+from tqdm import tqdm
+import mlflow.pytorch
+
+from src.data import TrainValTestSplitter, MURASubset
+from src.data.transforms import GrayScale, Padding, Resize, HistEqualisation, MinMaxNormalization, ToTensor
+from src.models.vae-pytorch import VAE
+from src.models.torchsummary import summary
+from src import XR_HAND_CROPPED_PATH, MODELS_DIR, MLFLOW_TRACKING_URI, XR_HAND_PATH
+
+# Ignoring numpy warnings and setting seeds
+np.seterr(divide='ignore', invalid='ignore')
+torch.manual_seed(42)
+
+# General Setup
+model_class = VAE
+device = "cuda" if torch.cuda.is_available() else "cpu"
+num_workers = 6
+
+# Mlflow settings
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+mlflow.set_experiment(model_class.__name__)
+
+# Mlflow parameters
+run_params = {
+    'batch_size': 64,
+    'image_resolution': (512, 512),
+    'num_epochs': 500,
+    'batch_normalisation': True,
+    'pipeline': {
+        'hist_equalisation': False,
+        'cropped': True,
+    }
+}
+
+# Initialization
+data_path = XR_HAND_CROPPED_PATH if run_params['pipeline']['cropped'] else XR_HAND_PATH
+
+print(f'\nDATA SPLIT:')
+splitter = TrainValTestSplitter(path_to_data=data_path)
+
+composed_transforms = Compose([GrayScale(),
+                               Padding(),
+                               Resize(run_params['image_resolution']),
+                               HistEqualisation(active=run_params['pipeline']['hist_equalisation']),
+                               MinMaxNormalization(),
+                               ToTensor()])
+
+train = MURASubset(filenames=splitter.data_train.path, patients=splitter.data_train.patient,
+                   transform=composed_transforms)
+validation = MURASubset(filenames=splitter.data_val.path, true_labels=splitter.data_val.label,
+                        patients=splitter.data_val.patient, transform=composed_transforms)
+test = MURASubset(filenames=splitter.data_test.path, true_labels=splitter.data_test.label,
+                  patients=splitter.data_test.patient, transform=composed_transforms)
+
+train_loader = DataLoader(train, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers)
+val_loader = DataLoader(validation, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers)
+test_loader = DataLoader(test, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers)
+
+model = model_class(use_batchnorm=run_params['batch_normalisation']).to(device)
+# model = torch.load(f'{MODELS_DIR}/{current_model.__name__}.pt')
+# model.eval().to(device)
+print(f'\nMODEL ARCHITECTURE:')
+model_summary, trainable_params = summary(model, input_size=(1, *run_params['image_resolution']), device=device)
+run_params['trainable_params'] = trainable_params
+
+# Logging
+print(f'\nRUN PARAMETERS: {run_params}')
+for (param, value) in run_params.items():
+    mlflow.log_param(param, value)
+
+# Training
+inner_loss = nn.MSELoss()
+outer_loss = nn.MSELoss(reduction='none')
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+for epoch in range(run_params['num_epochs']):
+
+    print('===========Epoch [{}/{}]============'.format(epoch + 1, run_params['num_epochs']))
+
+    for batch_data in tqdm(train_loader, desc='Training', total=len(train_loader)):
+        model.train()
+        inp = Variable(batch_data['image']).to(device)
+
+        # forward pass
+        output = model(inp)
+        loss = inner_loss(output, inp)
+
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # log
+    print(f'Loss on last train batch: {loss.data}')
+
+    # validation
+    val_metrics = model.evaluate(val_loader, 'validation', outer_loss, device, log_to_mlflow=True)
+
+    # forward pass for the random validation image
+    index = np.random.randint(0, len(validation), 1)[0]
+    model.forward_and_save_one_image(validation[index]['image'].unsqueeze(0), validation[index]['label'], epoch, device)
+
+print('=========Training ended==========')
+
+# Test performance
+model.evaluate(test_loader, 'test', outer_loss, device, log_to_mlflow=True, opt_threshold=val_metrics['optimal mse threshold'])
+
+# Saving
+mlflow.pytorch.log_model(model, 'current_model.__name__')
+torch.save(model, f'{MODELS_DIR}/{model_class.__name__}.pth')
