09709c76233ff81789442ccb46c7ea9c8d260681
Hitansh Singla <hitansh.singla@campus.lmu.de>
2019-06-12 15:25:03 +0200

    Update xml_to_csv.py

--- src/model_creation/preprocessing/xml_to_csv.py
+++ src/model_creation/preprocessing/xml_to_csv.py
@@ -32,15 +32,7 @@ def xml_to_csv(path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
-            value = (root.find('filename').text,
-                    int(root.find('size')[0].text),
-                    int(root.find('size')[1].text),
-                    member[0].text,
-                    int(member[4][0].text),
-                    int(member[4][1].text),
-                    int(member[4][2].text),
-                    int(member[4][3].text)
-                    )
+            value = (root.find('filename').text,int(root.find('size')[0].text),int(root.find('size')[1].text),member[0].text,int(member[4][0].text),int(member[4][1].text),int(member[4][2].text),int(member[4][3].text))
             xml_list.append(value)
     column_name = ['filename', 'width', 'height',
                 'class', 'xmin', 'ymin', 'xmax', 'ymax']
