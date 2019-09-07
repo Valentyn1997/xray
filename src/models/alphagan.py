Corrected normalisation, add Minibatch discrimination

src/models/alphagan.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import torchvision.utils as vutils
 from torch.autograd import Variable
+from torchgan.layers import MinibatchDiscrimination1d
 from tqdm import tqdm
 
 from src import TMP_IMAGES_DIR, TOP_K
@@ -257,6 +258,7 @@ class Discriminator(nn.Module):
         self.l1 = nn.Sequential(*layer1)
         self.l2 = nn.Sequential(*layer2)
         self.l3 = nn.Sequential(*layer3)
+        self.minibatch_discrimination = MinibatchDiscrimination1d(in_features=curr_dim, out_features=curr_dim)
 
         last.append(SpectralNorm(nn.Conv2d(curr_dim, 1, 4)))
         last.append(nn.Sigmoid())
@@ -278,6 +280,8 @@ class Discriminator(nn.Module):
         x3, p1 = self.attn1(x)
         x = self.l5(x3)
         x4, p2 = self.attn2(x)
+        # batch_data = self.minibatch_discrimination(x4)
+        # x4 = torch.cat(x4, batch_data)
         x5 = self.last(x4)
 
         return x5.squeeze(), x4, x3, p1, p2
@@ -493,7 +497,7 @@ class AlphaGan(nn.Module):
                 'discriminator loss': float(self.d_loss),
                 'codiscriminator loss': float(self.c_loss)}
 
-    def visualize_generator(self, epoch, to_mlflow=False, is_remote=False, *args, **kwargs):
+    def visualize_generator(self, epoch, to_mlflow=False, is_remote=False, vmin=0, vmax=1, *args, **kwargs):
         # Check how the generator is doing by saving G's output on fixed_noise
         path = TMP_IMAGES_DIR
         # Evaluation mode
@@ -501,7 +505,7 @@ class AlphaGan(nn.Module):
 
         with torch.no_grad():
             fake = self.generator(self.fixed_noise)[0].detach().cpu()
-            img = vutils.make_grid(fake, padding=20, normalize=False)
+            img = vutils.make_grid(fake, padding=20, normalize=False, range=(vmin, vmax))
             path = f'{path}/epoch{epoch}.png'
             vutils.save_image(img, path)
 
@@ -584,7 +588,7 @@ class AlphaGan(nn.Module):
         save_model(self, log_to_mlflow=True, is_remote=is_remote)
 
     def forward_and_save_one_image(self, inp_image, label, epoch, path=TMP_IMAGES_DIR, to_mlflow=False,
-                                   is_remote=False):
+                                   is_remote=False, vmin=0, vmax=1):
         """
         Reconstructs one image and writes two images (original and reconstructed) in one figure to :param path.
         :param inp_image: Image for evaluation
@@ -609,8 +613,8 @@ class AlphaGan(nn.Module):
             inp_image = inp_image.to('cpu')
             x_rec = x_rec.to('cpu')
             fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
-            ax[0].imshow(inp_image.numpy()[0, 0, :, :], cmap='gray', vmin=0, vmax=1)
-            ax[1].imshow(x_rec.numpy()[0, 0, :, :], cmap='gray', vmin=0, vmax=1)
+            ax[0].imshow(inp_image.numpy()[0, 0, :, :], cmap='gray', vmin=vmin, vmax=vmax)
+            ax[1].imshow(x_rec.numpy()[0, 0, :, :], cmap='gray', vmin=vmin, vmax=vmax)
             path = f'{path}/epoch{epoch}_label{int(label)}.png'
             plt.savefig(path)
             plt.close(fig)

src/models/gans.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torchvision.utils as vutils
 from torch.distributions.uniform import Uniform
 from torch.nn import Parameter
+from torchgan.layers import MinibatchDiscrimination1d
 from tqdm import tqdm
 
 from src import TMP_IMAGES_DIR
@@ -94,8 +95,8 @@ class Generator(nn.Module):
                  decoder_paddings: List[int] = (0, 1, 1, 1, 1, 1, 1, 1),
                  batch_normalisation=True,
                  spectral_normalisation=True,
-                 internal_activation=nn.ReLU,
-                 final_activation=nn.Sigmoid):
+                 internal_activation=nn.LeakyReLU,
+                 final_activation=nn.Tanh):
         super(Generator, self).__init__()
         self.hyper_parameters = locals()
         self.hyper_parameters.pop('self')
@@ -132,13 +133,13 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self,
                  encoder_in_chanels: List[int] = (1, 4, 8, 16, 32, 64, 128, 256),
-                 encoder_out_chanels: List[int] = (4, 8, 16, 32, 64, 128, 256, 1),
+                 encoder_out_chanels: List[int] = (4, 8, 16, 32, 64, 128, 256, 512),
                  encoder_kernel_sizes: List[int] = (4, 4, 4, 4, 4, 4, 4, 4, 4),
                  encoder_strides: List[int] = (2, 2, 2, 2, 2, 2, 2, 1),
                  encoder_paddings: List[int] = (1, 1, 1, 1, 1, 1, 1, 0),
                  batch_normalisation: bool = True,
                  spectral_normalisation=True,
-                 internal_activation=nn.ReLU,
+                 internal_activation=nn.LeakyReLU,
                  final_activation=nn.Sigmoid):
         super(Discriminator, self).__init__()
         self.hyper_parameters = locals()
@@ -159,15 +160,24 @@ class Discriminator(nn.Module):
 
             if batch_normalisation:
                 self.encoder_layers.append(nn.BatchNorm2d(encoder_out_chanels[i]))
-            if i < len(encoder_in_chanels) - 1:
-                self.encoder_layers.append(internal_activation())
-            else:
-                self.encoder_layers.append(final_activation())
+            # if i < len(encoder_in_chanels) - 1:
+            self.encoder_layers.append(internal_activation())
+            # else:
+            #     self.encoder_layers.append(final_activation())
 
         self.discriminator = nn.Sequential(*self.encoder_layers)
 
+        self.fc = nn.Linear(512 + 16, 1)
+        self.final_activation = final_activation()
+        self.minibatch_discrimination = MinibatchDiscrimination1d(in_features=512 * 4, out_features=16 * 4,
+                                                                  intermediate_features=128)
+
     def forward(self, x):
-        return self.discriminator(x)
+        x = self.discriminator(x)
+        x_flat = x.view(-1, 512 * 4)
+        x = self.minibatch_discrimination(x_flat).view(x.shape[0], -1)
+        x = self.fc(x)
+        return self.final_activation(x)
 
 
 class DCGAN(nn.Module):
@@ -237,11 +247,11 @@ class DCGAN(nn.Module):
         print('Generator:')
         model_summary, trainable_paramsG = summary(self.generator, input_size=(self.z_dim, 1, 1), device=self.device)
         print('Discriminator:')
-        model_summary, trainable_paramsD = summary(self.discriminator, input_size=(1, *image_resolution),
-                                                   device=self.device)
-        return trainable_paramsG + trainable_paramsD
+        # model_summary, trainable_paramsD = summary(self.discriminator, input_size=(16, *image_resolution),
+        #                                            device=self.device)
+        return trainable_paramsG  # + trainable_paramsD
 
-    def train_on_batch(self, batch_data, epoch, *args, **kwargs):
+    def train_on_batch(self, batch_data, epoch, num_epochs, *args, **kwargs):
         """
         Performs one step of gradient descent on batch_data
         :param batch_data: Data of batch
@@ -262,6 +272,8 @@ class DCGAN(nn.Module):
             label += self.real_labels_softener.sample((b_size,)).to(self.device)
 
         # Backward pass (1) Update discriminator network: maximize log(D(x)) + log(1 - D(G(z)))
+        # noise1 = torch.Tensor(real_inp.size()).normal_(0, 0.01 * (epoch + 1 - num_epochs) / (
+        #         epoch + 1)).to(self.device)  # Noise for real images
         self.discriminator.zero_grad()
         output = self.discriminator(real_inp).view(-1)
         # Train with all-real batch
@@ -273,6 +285,9 @@ class DCGAN(nn.Module):
         label.fill_(self.fake_label)
         if self.soft_labels:
             label += self.fake_labels_softener.sample((b_size,)).to(self.device)
+
+        # noise2 = torch.Tensor(real_inp.size()).normal_(0, 0.01 * (epoch + 1 - num_epochs) / (
+        #         epoch + 1)).to(self.device)  # Noise for fake images
         output = self.discriminator(fake.detach()).view(-1)  # detached fake - not to backprop on generator
         loss_D_fake = self.inner_loss(output, label)
         # Calculate the gradients for this batch
@@ -298,7 +313,7 @@ class DCGAN(nn.Module):
         return {'generator loss': float(self.loss_G),
                 'discriminator loss': float(self.loss_D)}
 
-    def visualize_generator(self, epoch, to_mlflow=False, is_remote=False, *args, **kwargs):
+    def visualize_generator(self, epoch, to_mlflow=False, is_remote=False, vmin=-1, vmax=1, *args, **kwargs):
         # Check how the generator is doing by saving G's output on fixed_noise
         path = TMP_IMAGES_DIR
         # Evaluation mode
@@ -306,7 +321,7 @@ class DCGAN(nn.Module):
 
         with torch.no_grad():
             fake = self.generator(self.fixed_noise).detach().cpu()
-        img = vutils.make_grid(fake, padding=20, normalize=False)
+        img = vutils.make_grid(fake, padding=20, normalize=False, range=(vmin, vmax))
         path = f'{path}/epoch{epoch}.png'
         vutils.save_image(img, path)
 

src/models/sagan.py
@@ -481,14 +481,15 @@ class SAGAN(nn.Module):
         return {'generator-encoder loss': float(self.ge_loss),
                 'discriminator loss': float(self.d_loss)}
 
-    def visualize_generator(self, epoch, path=TMP_IMAGES_DIR, to_mlflow=False, is_remote=False, *args, **kwargs):
+    def visualize_generator(self, epoch, path=TMP_IMAGES_DIR, to_mlflow=False, is_remote=False, vmin=0, vmax=1, *args,
+                            **kwargs):
         # Check how the generator is doing by saving G's output on fixed_noise
         # Evaluation mode
         self.generator.eval()
 
         with torch.no_grad():
             fake = self.generator(self.fixed_noise)[0].detach().cpu()
-            img = vutils.make_grid(fake, padding=20, normalize=False)
+            img = vutils.make_grid(fake, padding=20, normalize=False, range=(vmin, vmax))
             img_path = f'{path}/epoch{epoch}.png'
             vutils.save_image(img, img_path)
 
@@ -497,7 +498,7 @@ class SAGAN(nn.Module):
                 os.remove(img_path)
 
     def forward_and_save_one_image(self, inp_image, label, epoch, path=TMP_IMAGES_DIR, to_mlflow=False,
-                                   is_remote=False):
+                                   is_remote=False, vmin=0, vmax=1):
         """
         Reconstructs one image and writes two images (original and reconstructed) in one figure to :param path.
         :param is_remote:
@@ -521,8 +522,8 @@ class SAGAN(nn.Module):
             output_img = reconstructed_img.to('cpu')
 
             fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
-            ax[0].imshow(inp_image.numpy()[0, 0, :, :], cmap='gray', vmin=0, vmax=1)
-            ax[1].imshow(output_img.numpy()[0, 0, :, :], cmap='gray', vmin=0, vmax=1)
+            ax[0].imshow(inp_image.numpy()[0, 0, :, :], cmap='gray', vmin=vmin, vmax=vmax)
+            ax[1].imshow(output_img.numpy()[0, 0, :, :], cmap='gray', vmin=vmin, vmax=vmax)
             path = f'{path}/epoch{epoch}_label{int(label)}.png'
             plt.savefig(path)
             plt.close(fig)

src/models/train.py
@@ -24,7 +24,7 @@ from src.utils import query_yes_no, save_model
 
 # ---------------------------------------  Parameters setups ---------------------------------------
 # set model type
-model_class = SAGAN
+model_class = DCGAN
 
 # set device
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -40,7 +40,7 @@ remote_run = query_yes_no('Is this run remote?', 'no')
 if log_to_mlflow:
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     mlflow.set_experiment(model_class.__name__ + 'New')
-    mlflow.start_run(run_id='553225ba44224347a60a34154e9f7ecf')
+    mlflow.start_run()
 
 # Ignoring numpy warnings
 np.seterr(divide='ignore', invalid='ignore')
@@ -70,7 +70,7 @@ for random_seed in run_params['random_seed']:
                                    Resize(run_params['image_resolution'], keep_aspect_ratio=True),
                                    Padding(max_shape=run_params['image_resolution']),
                                    # max_shape - max size of image after augmentation
-                                   MinMaxNormalization(),
+                                   MinMaxNormalization(*run_params['pipeline']['normalisation']),
                                    ToTensor()])
 
     # transformation for validation and test
@@ -82,7 +82,7 @@ for random_seed in run_params['random_seed']:
                                        Resize(run_params['image_resolution'], keep_aspect_ratio=True),
                                        Padding(max_shape=run_params['image_resolution']),
                                        # max_shape - max size of image after augmentation
-                                       MinMaxNormalization(),
+                                       MinMaxNormalization(*run_params['pipeline']['normalisation']),
                                        ToTensor()])
 
     # Dataset loaders
@@ -100,9 +100,11 @@ for random_seed in run_params['random_seed']:
                       patients=splitter.data_test.patient, transform=composed_transforms_val)
 
     train_loader = DataLoader(train, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers,
-                              worker_init_fn=loader_init_fn)
-    val_loader = DataLoader(validation, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers)
-    test_loader = DataLoader(test, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers)
+                              worker_init_fn=loader_init_fn, drop_last=True)
+    val_loader = DataLoader(validation, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers,
+                            drop_last=True)
+    test_loader = DataLoader(test, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers,
+                             drop_last=True)
 
     # Model initialization
     model = model_class(device=device, **run_params)
@@ -162,15 +164,21 @@ for random_seed in run_params['random_seed']:
             model.forward_and_save_one_image(validation[index]['image'].unsqueeze(0),
                                              validation[index]['label'], epoch, to_mlflow=log_to_mlflow,
                                              is_remote=remote_run)
-        elif model_class in [DCGAN] and epoch % 4 == 0:
+        elif model_class in [DCGAN] and epoch % 3 == 0:
             # evaluate performance of generator
-            model.visualize_generator(epoch, to_mlflow=log_to_mlflow, is_remote=remote_run)
+            model.visualize_generator(epoch, to_mlflow=log_to_mlflow, is_remote=remote_run,
+                                      vmin=run_params['pipeline']['normalisation'][0],
+                                      vmax=run_params['pipeline']['normalisation'][1])
         elif model_class in [AlphaGan, SAGAN] and epoch % 2 == 0:
             # evaluate performance of generator
-            model.visualize_generator(epoch, to_mlflow=log_to_mlflow, is_remote=remote_run)
+            model.visualize_generator(epoch, to_mlflow=log_to_mlflow, is_remote=remote_run,
+                                      vmin=run_params['pipeline']['normalisation'][0],
+                                      vmax=run_params['pipeline']['normalisation'][1])
             # evaluate performance of encoder / generator
             model.forward_and_save_one_image(validation[index]['image'].unsqueeze(0), validation[index]['label'], epoch,
-                                             to_mlflow=log_to_mlflow, is_remote=remote_run)
+                                             to_mlflow=log_to_mlflow, is_remote=remote_run,
+                                             vmin=run_params['pipeline']['normalisation'][0],
+                                             vmax=run_params['pipeline']['normalisation'][1])
 
         # Checkpoints
         if 'checkpoint_frequency' in run_params and epoch % run_params['checkpoint_frequency'] == 0: