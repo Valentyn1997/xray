Minibatch discrimination for AlphaGAN, correct normalisation for AE, extended VAE evaluation

src/models/alphagan.py
@@ -258,9 +258,12 @@ class Discriminator(nn.Module):
         self.l1 = nn.Sequential(*layer1)
         self.l2 = nn.Sequential(*layer2)
         self.l3 = nn.Sequential(*layer3)
-        self.minibatch_discrimination = MinibatchDiscrimination1d(in_features=curr_dim, out_features=curr_dim)
+        md_out_channels = 4
+        self.minibatch_discrimination = MinibatchDiscrimination1d(in_features=curr_dim * 4 * 4,
+                                                                  out_features=md_out_channels * 4 * 4,
+                                                                  intermediate_features=256)
 
-        last.append(SpectralNorm(nn.Conv2d(curr_dim, 1, 4)))
+        last.append(SpectralNorm(nn.Conv2d(curr_dim + md_out_channels, 1, 4)))
         last.append(nn.Sigmoid())
         self.last = nn.Sequential(*last)
 
@@ -282,8 +285,9 @@ class Discriminator(nn.Module):
         x4, p2 = self.attn2(x)
         # batch_data = self.minibatch_discrimination(x4)
         # x4 = torch.cat(x4, batch_data)
+        x_flat = x4.view(-1, 1024 * 4 * 4)
+        x4 = self.minibatch_discrimination(x_flat).view(x4.shape[0], -1, 4, 4)
         x5 = self.last(x4)
-
         return x5.squeeze(), x4, x3, p1, p2
 
 
@@ -366,7 +370,7 @@ class AlphaGan(nn.Module):
         self.d_optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.discriminator.parameters()), dlr,
                                             betas=(0.5, 0.999))
         self.c_optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.codescriminator.parameters()),
-                                            0.1 * dlr,
+                                            0.01 * dlr,
                                             betas=(0.5, 0.999))
 
         # Placeholders for losses of disriminator and generator

src/models/train.py
@@ -24,10 +24,11 @@ from src.utils import query_yes_no, save_model
 
 # ---------------------------------------  Parameters setups ---------------------------------------
 # set model type
-model_class = DCGAN
+model_class = VAE
 
 # set device
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f'Device: {device}')
 torch.cuda.empty_cache()
 # torch.cuda.set_device(1)
 # device = 'cpu'
@@ -100,19 +101,14 @@ for random_seed in run_params['random_seed']:
                       patients=splitter.data_test.patient, transform=composed_transforms_val)
 
     train_loader = DataLoader(train, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers,
-                              worker_init_fn=loader_init_fn, drop_last=True)
+                              worker_init_fn=loader_init_fn, drop_last=model_class in [DCGAN])
     val_loader = DataLoader(validation, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers,
-                            drop_last=True)
+                            drop_last=model_class in [DCGAN])
     test_loader = DataLoader(test, batch_size=run_params['batch_size'], shuffle=True, num_workers=num_workers,
-                             drop_last=True)
+                             drop_last=model_class in [DCGAN])
 
     # Model initialization
     model = model_class(device=device, **run_params)
-
-    # Parallelism
-    if torch.cuda.device_count() > 1:
-        model.parallelize()
-
     model = model.to(device)
 
     # model = torch.load(f'{MODELS_DIR}/{model_class.__name__}.pth')
@@ -121,6 +117,10 @@ for random_seed in run_params['random_seed']:
     trainable_params = model.summary(image_resolution=run_params['image_resolution'])
     run_params['trainable_params'] = trainable_params
     run_params['other_hyperparams'] = model.hyper_parameters
+
+    # Parallelism
+    if torch.cuda.device_count() > 1:
+        model.parallelize()
 
     # -------------------------------- Logging ------------------------------------
     # Logging